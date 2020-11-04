91b13f0ec1c4cbd47783a90263e0491bc0a8f7f1
jferrazza <102116925@student.swin.edu.au>
2020-11-05 09:54:20 +1100

    further built topbar, put back buttons on dashboard, fixed a couple responsiveness clippings

--- src/app/fluid-drain/fluid-drain.component.ts
+++ src/app/fluid-drain/fluid-drain.component.ts
@@ -12,109 +12,111 @@ export class FluidDrainComponent implements OnInit {
   ngOnInit(): void {
 
     //when the page is loaded
-    setTimeout(() => {  {   
+    setTimeout(() => {
+      {
 
-      //declare function
-      function add(val){
-        //add this value to the existing value
-        $("#fluidInput").val( parseFloat($("#fluidInput").val())+val);
-        //update the value
-        $("#fluidScale").val($("#fluidInput").val());
-        //refresh
-        done();
-
-      }
-
-      var chosen = false;
-      //
-
-      //declare function
-      function done() {
-        //change the output value
-        $(".scale_selected").html($("#fluidInput").val());
-        //hide error
-        $(".invalidNumber").css("display", "none");
-      }
-
-      //if not chosen earlier
-      if (!chosen) {
-        window.onbeforeunload = function () {
-          return true;
-        };
-        //validate the form
-        $(".scale_showSelected.alert").removeClass("alert-danger");
-        $(".scale_showSelected.alert").addClass("alert-primary");
-
-        //alert unsaved changes
-        $(".scale_back").html($(".scale_back").html() + "*");
-      }
-
-      //clear selection
-      $(".scale_btns .btn").removeClass("scale_this");
-
-      //show selection
-      $(this).addClass("scale_this");
-      chosen = true;
-      $(".scale_showSelected").html('You have picked <span class="scale_selected"></span>mL.');
-
-      //validate/validation-reset the form
-      $(".scale_submit").prop('disabled', false);
-      $("#invalidNumber").css("display", "none");
-
-      //on value change
-      $("#fluidScale").change(function () {
-
-        //if the values are different, make them the same
-        if ($("#fluidInput").val() != $("#fluidScale").val())
-          $("#fluidInput").val($("#fluidScale").val());
-        done();
-      });
-
-      //on value change
-      $("#fluidInput").change(function () {
-
-        //if the value is undefined or less than zero
-        if (isNaN(parseFloat($("#fluidInput").val())) || parseFloat($("#fluidInput").prop("min")) > parseFloat($("#fluidInput").val())) {
-
-          //reset the input fields
-          $("#fluidInput").val($("#fluidScale").val());
-          //reset the output fields
+        //declare function
+        function add(val) {
+          //add this value to the existing value
+          $("#fluidInput").val(parseFloat($("#fluidInput").val()) + val);
+          //update the value
+          $("#fluidScale").val($("#fluidInput").val());
+          //refresh
           done();
-          //throw error (unhide all error alerts)
-          $(".invalidNumber").css("display", "");
-          //quit $.change
-          return;
+
         }
 
-        //if the values are different, make them the same
-        if ($("#fluidInput").val() != $("#fluidScale").val())
-          $("#fluidScale").val($("#fluidInput").val());
+        var chosen = false;
+        //
+
+        //declare function
+        function done() {
+          //change the output value
+          $(".scale_selected").html($("#fluidInput").val());
+          //hide error
+          $(".invalidNumber").css("display", "none");
+        }
+
+        //if not chosen earlier
+        if (!chosen) {
+          window.onbeforeunload = function () {
+            return true;
+          };
+          //validate the form
+          $(".scale_showSelected.alert").removeClass("alert-danger");
+          $(".scale_showSelected.alert").addClass("alert-primary");
+
+          //alert unsaved changes
+          $(".scale_back").html($(".scale_back").html() + "*");
+        }
+
+        //clear selection
+        $(".scale_btns .btn").removeClass("scale_this");
+
+        //show selection
+        $(this).addClass("scale_this");
+        chosen = true;
+        $(".scale_showSelected").html('You have picked <span class="scale_selected"></span>mL.');
+
+        //validate/validation-reset the form
+        $(".scale_submit").prop('disabled', false);
+        $("#invalidNumber").css("display", "none");
+
+        //on value change
+        $("#fluidScale").change(function () {
+
+          //if the values are different, make them the same
+          if ($("#fluidInput").val() != $("#fluidScale").val())
+            $("#fluidInput").val($("#fluidScale").val());
+          done();
+        });
+
+        //on value change
+        $("#fluidInput").change(function () {
+
+          //if the value is undefined or less than zero
+          if (isNaN(parseFloat($("#fluidInput").val())) || parseFloat($("#fluidInput").prop("min")) > parseFloat($("#fluidInput").val())) {
+
+            //reset the input fields
+            $("#fluidInput").val($("#fluidScale").val());
+            //reset the output fields
+            done();
+            //throw error (unhide all error alerts)
+            $(".invalidNumber").css("display", "");
+            //quit $.change
+            return;
+          }
+
+          //if the values are different, make them the same
+          if ($("#fluidInput").val() != $("#fluidScale").val())
+            $("#fluidScale").val($("#fluidInput").val());
+          done();
+        });
+
+        //
+
+        //force value change on page load
+        $("#fluidInput").val($("#fluidScale").val());
+        //update the output fields
         done();
-      });
-
-      //
-
-      //force value change on page load
-      $("#fluidInput").val($("#fluidScale").val());
-      //update the output fields
-      done();
 
 
-      //make buttons call function
-      $("#btnPlus").click(function(){
-        add(1);
-      });
-      $("#btnMinus").click(function(){
-        add(-1);
-      });
+        //make buttons call function
+        $("#btnPlus").click(function () {
+          add(1);
+        });
+        $("#btnMinus").click(function () {
+          add(-1);
+        });
 
-      //on error dismiss call function
-      $("#btnDismiss").click(function(){
-        done();
-      });
+        //on error dismiss call function
+        $("#btnDismiss").click(function () {
+          done();
+        });
 
-      //end of $.on
-    }},100);
+        //end of $.on
+      }
+    }, 100);
 
   }
 
--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -9,29 +9,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  @Input() strTitle: string = "Enter Title";
-  @Input() strUser: string = "John";
-  @Input() strBackButtonTitle: string = "Home";
-  @Input() urlBackLink: string = "";
-  @Input() bolTransparent: boolean;
-  @Input() bolShowBackSpacer: boolean;
+
 
   constructor() { }
 
   ngOnInit(): void {
-    if (this.bolTransparent) {
-      console.warn("Necessary changes to DOM made for transparent titlebar.");
-      $(".nh_topbar").addClass('nh_home_hide');
-      $("._title").html("");
-
-    }
-    console.log(this.bolShowBackSpacer);
-    if (this.bolShowBackSpacer==false) {
-
-      console.warn("Necessary changes to DOM made for hidden titlebar spacer.");
-      $(".nh_topbar_back").css("display","none");
-    }
-
+   
   }
 
 }
